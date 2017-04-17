e48d621da59c26201a25be9c30ab48d2da16f793
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-04-17 16:21:23 +0200

    Initial commit working without thread

---
+++ SourceDownloader.py
@@ -0,0 +1,123 @@
+import urllib2
+import traceback
+from rdflib import Graph
+def getST(g,rlink):
+	st = 0
+	gt = 0
+	at = 0
+	ot = 0
+
+	fquery = ''' Select ?at ?st ?ot ?gt where {
+		  {SELECT (count(*) as ?zt) where {
+                    ?s ?p ?o .
+                  }}
+		  {SELECT (count(*) as ?st) where {
+                    ?s ?p ?o .
+                      FILTER (?s=<Resource>)
+                  }}
+                  {SELECT (count(*) as ?at) where {
+                    ?s ?p ?o .
+                  }}
+                  {SELECT (count(*) as ?ot) where {
+                    ?s ?p ?o .
+                      FILTER (?o=<Resource>)
+                  }}
+                  {SELECT (count(*) as ?gt) where {
+                    ?s ?p ?o .
+                      FILTER (?o!=<Resource> && ?s!=<Resource>)
+                  }}
+                } '''
+	query = fquery.replace("Resource",rlink.replace("\n",""))
+        result = g.query(query)
+        for p in result:
+                at = str(p.at)
+                st = str(p.st)
+                ot = str(p.ot)
+                gt = str(p.gt)
+        return [st,ot,gt]
+
+
+def setVal(sCases,st,ot,gt):
+	st = int(st)
+	ot = int(ot)
+	gt = int(gt)
+        if st == 0 and ot == 0 and gt > 0:
+                sCases["GT"]["NumCase"] = sCases["GT"]["NumCase"] + 1
+
+        elif st > 0 and ot == 0 and gt == 0:
+                sCases["ST"]["NumCase"] = sCases["ST"]["NumCase"] + 1
+
+        elif st == 0 and ot > 0 and gt == 0:
+                sCases["OT"]["NumCase"] = sCases["OT"]["NumCase"] + 1
+
+        elif st > 0 and ot > 0 and gt == 0:
+                sCases["ST,OT"]["NumCase"] = sCases["ST,OT"]["NumCase"] + 1
+
+        elif st > 0 and ot == 0 and gt > 0:
+                sCases["ST,GT"]["NumCase"] = sCases["ST,GT"]["NumCase"] + 1
+
+        elif st == 0 and ot > 0 and gt > 0:
+                sCases["OT,GT"]["NumCase"] = sCases["OT,GT"]["NumCase"] + 1
+
+        elif st > 0 and ot > 0 and gt > 0:
+                sCases["ST,OT,GT"]["NumCase"] = sCases["ST,OT,GT"]["NumCase"] + 1
+
+        elif st == 0 and ot == 0 and gt == 0:
+                sCases["Empty"]["NumCase"] = sCases["Empty"]["NumCase"] + 1
+
+        return sCases
+
+def evaluateRDF(fileurl,allstats):
+        try:
+                req = urllib2.Request(fileurl, headers={'Content-type': 'text/turtle,application/rdf+xml,text/ntriples,application/ld+json'})
+                response = urllib2.urlopen(req,timeout=4)
+                content = response.read()
+                contentType = response.info().getheader('Content-Type')
+                if "xml" in contentType:
+                        contentType = "xml"
+                elif "turtle" in contentType:
+                        contentType = "n3"
+                elif "n3" in contentType:
+                        contentType = "n3"
+                elif "nt" in contentType:
+                        contentType = "nt"
+                else:
+                        contentType = "xml"
+                g = Graph()
+                g.parse(data=content,format=contentType)
+                stats = getST(g,fileurl)
+                allstats = setVal(allstats,stats[0],stats[1],stats[2])
+		#print stats
+		#print allstats
+		return allstats
+
+        except:
+		traceback.print_exc()
+                return "error:" + " "+fileurl
+
+
+class Source:
+	sfile = ""
+
+	def __init__(self,sName,sCode,sNumSources,sfile,allStats):
+		self.sfile = sfile
+		self.f = open(sfile,"r")
+		self.allStats = allStats
+	
+	
+	def evaluate(self):
+		for url in self.f:
+			stats = evaluateRDF(url,self.allStats) 
+			#print url + str(stats)
+allstats = {
+          "GT":{"ST":"=0","OT":"=0","GT":">0","NumCase":0,"PNumCase":0},
+          "ST":{"ST":">0","OT":"=0","GT":"=0","NumCase":0,"PNumCase":0},
+          "OT":{"ST":"=0","OT":">0","GT":"=0","NumCase":0,"PNumCase":0},
+          "ST,OT":{"ST":">0","OT":">0","GT":"=0","NumCase":0,"PNumCase":0},
+          "ST,GT":{"ST":">0","OT":"=0","GT":">0","NumCase":0,"PNumCase":0},
+          "OT,GT":{"ST":"=0","OT":">0","GT":">0","NumCase":0,"PNumCase":0},
+          "ST,OT,GT":{"ST":">0","OT":">0","GT":">0","NumCase":0,"PNumCase":0},
+          "Empty":{"ST":"=0","OT":"=0","GT":"=0","NumCase":0,"PNumCase":0}
+          }
+#evaluateRDF("http://www.semanlink.net/tag/mp3.rdf",allstats)
+#evaluateRDF("http://www.semanlink.net/tag/mp3.rdf",allstats)
---
+++ run.py
@@ -0,0 +1,33 @@
+from SourceDownloader import Source
+import urllib2
+from rdflib import Graph
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+allstats = {
+          "GT":{"ST":"=0","OT":"=0","GT":">0","NumCase":0,"PNumCase":0},
+          "ST":{"ST":">0","OT":"=0","GT":"=0","NumCase":0,"PNumCase":0},
+          "OT":{"ST":"=0","OT":">0","GT":"=0","NumCase":0,"PNumCase":0},
+          "ST,OT":{"ST":">0","OT":">0","GT":"=0","NumCase":0,"PNumCase":0},
+          "ST,GT":{"ST":">0","OT":"=0","GT":">0","NumCase":0,"PNumCase":0},
+          "OT,GT":{"ST":"=0","OT":">0","GT":">0","NumCase":0,"PNumCase":0},
+          "ST,OT,GT":{"ST":">0","OT":">0","GT":">0","NumCase":0,"PNumCase":0},
+          "Empty":{"ST":"=0","OT":"=0","GT":"=0","NumCase":0,"PNumCase":0}
+          }
+
+directory = "/home/bakerally/Documents/repositories/emse_gitlab/LODRDFAnalysis/test/"
+f = open(directory+"repstat","r")
+for line in f:
+	if ">" in line:
+		continue
+	lineParts = line.split(",")
+	sourceName = lineParts[0]
+	code = lineParts[1]
+	numSources = lineParts[2]
+	sourceFilename = directory + code
+	
+	s = Source(sourceName,code,numSources,sourceFilename,allstats)
+	s.evaluate() 
+
+print allstats
