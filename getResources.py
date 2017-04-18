ff3867ef821cae5a05bdc4616d4f0756dffe8326
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-04-18 13:48:55 +0000

    not added removed

--- getResources.py
+++ getResources.py
@@ -92,7 +92,6 @@ def addResource(rURL,role):
 
 def iterTriples():
 	f = open(sys.argv[1],"r")
-	err = open("notAdded","w")
 	tp = 1
 	for line in f:
 		t = line.split(" ")	
@@ -102,13 +101,8 @@ def iterTriples():
 		o = o[1:len(o)-1]
 		if (validators.url(s)):
 			addResource(s,"subject")
-		else:
-			err.write(s+"\n")
 		if (validators.url(o)):
 			addResource(o,"object")
-		else:
-			err.write(o+"\n")
-		print
 		#print "Triples Processed:"+str(tp)
 		#print "Resources Added:"+str(numResources)
 		counter = open("counter","w")
