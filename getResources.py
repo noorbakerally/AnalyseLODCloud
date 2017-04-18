60d03b9d603c0370ff766b5025535381121115eb
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-04-19 00:27:15 +0200

    urllib2 added

--- getResources.py
+++ getResources.py
@@ -5,6 +5,7 @@ from urlparse import urlparse
 import validators
 from datetime import datetime
 import sys
+import urllib2
 
 numResources = 0
 dindex = {}
@@ -72,7 +73,7 @@ def addURL(rURL,role):
 	global durls
 	URLKey,namespace,sep,sepType = getURLKey(rURL)
 	if URLKey not in durls.keys():
-		durls[URLKey] = {"subject":0,"object":0,"sep":sep,"sepType":sepType,"sampleURL":rURL}
+		durls[URLKey] = {"subject":0,"predicate":0,"object":0,"sep":sep,"sepType":sepType,"sampleURL":rURL}
 	durls[URLKey][role] =  durls[URLKey][role] + 1	
 	
 
@@ -91,20 +92,23 @@ def addResource(rURL,role):
 		numResources = numResources + 1
 
 def iterTriples():
-	f = open(sys.argv[1],"r")
+	#f = open("test.nq","r")
+	f = urllib2.urlopen("http://ci.emse.fr/dump/dmp/dump.nq")
 	tp = 1
 	for line in f:
 		t = line.split(" ")	
 		s = t[0]
 		s = s[1:len(s)-1]
+		p = t[1]
+		p = p[1:len(p)-1]
 		o = t[2]
 		o = o[1:len(o)-1]
 		if (validators.url(s)):
 			addResource(s,"subject")
 		if (validators.url(o)):
 			addResource(o,"object")
-		#print "Triples Processed:"+str(tp)
-		#print "Resources Added:"+str(numResources)
+		addURL(p,"predicate")
+		print "Triples Processed:"+str(tp)+" Resources Added:"+str(numResources)
 		counter = open("counter","w")
 		counter.write(str(tp)+"\n")
 		counter.close()
